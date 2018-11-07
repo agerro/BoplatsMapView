7612b52de576355c2c1a9571832831200ad4da7a
Marcus.A.Karlsson <Marcus.A.Karlsson@cybercom.com>
2018-11-08 00:42:36 +0100

    Updated with better performance

--- js/boplats.js
+++ js/boplats.js
@@ -1,13 +1,22 @@
 window.onload = function () {
+
     var addresses = document.getElementById("objectlist").getElementsByClassName("address");
     for (var i = 0; i < addresses.length; i++) {
 
-        var searchterm = encodeURI(addresses[i].innerHTML);
+        addresses[i].onmouseover = function () {
+            var searchterm = encodeURI(this.innerHTML);
 
-        var map = document.createElement('div');
-        map.className = 'mapouter';
-        map.innerHTML = '<div class="gmap_canvas"><iframe width="100%" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=' + searchterm + '&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.crocothemes.net"></a></div><style>.mapouter{text-align:right;height:200px;width:100%;margin:0 !important;}.gmap_canvas {overflow:hidden;background:none!important;height:200px;width:100%;margin:0 !important;}</style>'
+            var map = document.createElement('div');
+            map.className = 'mapouter';
+            map.innerHTML = '<div class="gmap_canvas"><iframe width="100%" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=' + searchterm + '&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{text-align:right;height:200px;width:100%;margin:0 !important;}.gmap_canvas {overflow:hidden;background:none!important;height:200px;width:100%;margin:0 !important;}</style>'
 
-        document.getElementById("objectlist").getElementsByClassName("address")[i].parentNode.parentNode.parentNode.parentNode.appendChild(map);
+            this.parentNode.parentNode.parentNode.parentNode.appendChild(map);
+        }
+        addresses[i].onmouseout = function () {
+            var map = document.getElementsByClassName('mapouter');
+            while (map.length > 0) {
+                this.parentNode.parentNode.parentNode.parentNode.removeChild(map[0])
+            }
+        }
     }
 }
